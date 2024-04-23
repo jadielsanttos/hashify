f0f787d937f2d79fe69e53813e1efc934937c7f3
Jadiel WSL2 <jadielsilva11406@gmail.com>
2024-04-22 22:00:23 -0300

    resolving KeyBoardInterrupt error

--- app.py
+++ app.py
@@ -13,28 +13,34 @@ print("|[1] -> MD5           |")
 print("|[2] -> SHA256        |")
 print("#=====================#"+"\n")
 
-option = input("Choose an option: ")
-text = input("Enter your text: ")
-
-def hash_generate(text, option):
-    encode = text.encode("UTF-8")
-
+def hash_generate():
     hash = ""
+    try:
+        while True:
+            option = input("Choose an option: ")
+            text = input("Enter your text: ")
+            encode = text.encode("UTF-8")
 
-    if option == "1":
-        hash = hashlib.md5(encode).hexdigest()
-    else:
-        hash = hashlib.sha256(encode).hexdigest()
+            if option == "1":
+                hash = hashlib.md5(encode).hexdigest()
+            elif option == "2":
+                hash = hashlib.sha256(encode).hexdigest()
+            else:
+                print("Invalid option!")
+                break
 
-    hash_length = str(len(hash))
+            hash_length = str(len(hash))
 
-    print(" ")
-    for i in range(0, int(hash_length)):
-        print("=", end="")
-    print(" ")
-    print(hash)
-    for i in range(0, int(hash_length)):
-        print("=", end="")
-    print(" ")
+            print(" ")
+            for i in range(0, int(hash_length)):
+                print("=", end="")
+            print(" ")
+            print(hash)
+            for i in range(0, int(hash_length)):
+                print("=", end="")
+            print(" ")
+            
+    except KeyboardInterrupt:
+        print("\n Bye")
 
-hash_generate(text, option)
+hash_generate()
